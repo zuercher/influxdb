b7abf3349d592962cf23ca156f8f8480abe90ace
Jonathan A. Sternberg <jonathan@influxdb.com>
2017-02-08 08:09:13 -0600

    URL query parameter credentials take priority over Authentication header

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -63,6 +63,7 @@ The admin UI is removed and unusable in this release. The `[admin]` configuratio
 - [#8343](https://github.com/influxdata/influxdb/issues/8343): Set the CSV output to an empty string for null values.
 - [#8368](https://github.com/influxdata/influxdb/issues/8368): Compaction exhausting disk resources in InfluxDB
 - [#8358](https://github.com/influxdata/influxdb/issues/8358): Small edits to the etc/config.sample.toml file.
+- [#7957](https://github.com/influxdata/influxdb/issues/7957): URL query parameter credentials take priority over Authentication header.
 
 ## v1.2.3 [unreleased]
 
--- services/httpd/handler.go
+++ services/httpd/handler.go
@@ -986,6 +986,15 @@ type credentials struct {
 func parseCredentials(r *http.Request) (*credentials, error) {
 	q := r.URL.Query()
 
+	// Check for username and password in URL params.
+	if u, p := q.Get("u"), q.Get("p"); u != "" && p != "" {
+		return &credentials{
+			Method:   UserAuthentication,
+			Username: u,
+			Password: p,
+		}, nil
+	}
+
 	// Check for the HTTP Authorization header.
 	if s := r.Header.Get("Authorization"); s != "" {
 		// Check for Bearer token.
@@ -1005,15 +1014,6 @@ func parseCredentials(r *http.Request) (*credentials, error) {
 				Password: p,
 			}, nil
 		}
-	}
-
-	// Check for username and password in URL params.
-	if u, p := q.Get("u"), q.Get("p"); u != "" && p != "" {
-		return &credentials{
-			Method:   UserAuthentication,
-			Username: u,
-			Password: p,
-		}, nil
 	}
 
 	return nil, fmt.Errorf("unable to parse authentication credentials")
